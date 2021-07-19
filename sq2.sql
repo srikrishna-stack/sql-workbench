d24c89625dfe9ce91f1a14c4c7dba5da6821208b
sri krishna <82864734+srikrishna-stack@users.noreply.github.com>
2021-07-19 12:23:00 +0530

    Add files via upload

---
+++ sq2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM Student.Student1;
+INSERT INTO `Student`.`Student1` (`ROLL_NO`, `NAME`, `ADRESS`, `PHONE`, `AGE`) VALUES ('2', 'elon musk', 'new york', '12356', '22');
+INSERT INTO `Student`.`Student1` (`ROLL_NO`,`NAME`, `ADRESS`, `PHONE`,`AGE`)   VALUES ('3','bill','hjhvh','12345','24');
+select NAME from Student1;
